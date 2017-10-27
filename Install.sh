b1d7c614d4f91ff1481f7a22e3e3e1a97539415c
kicker22004 <kicker22004@gmail.com>
2017-10-27 16:31:45 -0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -22,10 +22,12 @@ fi
 apt-get -y update
 apt-get install -y screen git wget rsync unzip sysstat inotify-tools bc
 sudo apt-get -y upgrade
+if [ ! -f /usr/bin/himalaya ]; then
 sudo apt-get -y install npm node
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get -y install nodejs
 npm install --global himalaya
+fi
 #Grab JSON.sh from Dominictarr's Github. Thank you for this tool!!
 wget https://raw.githubusercontent.com/dominictarr/JSON.sh/master/JSON.sh -O JSON.sh
 
